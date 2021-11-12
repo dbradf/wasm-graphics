c1b31ac0805a659ec39ae408cd5bfed2fb84a3a0
David Bradford <david.bradford@mongodb.com>
2021-11-11 20:45:36 -0500

    Starting gh pages

--- README.md
+++
@@ -1,5 +0,0 @@
-# wasm-graphics
-
-Working through [Computer Graphics From Scratch](https://nostarch.com/computer-graphics-scratch) in
-Rust and webassembly.
-
--- index.js
+++
@@ -1,8 +0,0 @@
-import('./pkg')
-    .then(wasm => {
-        const canvas = document.getElementById('drawing');
-        const ctx = canvas.getContext('2d');
-
-        wasm.draw(ctx, 1000, 1000);
-    })
-    .catch(console.error);
